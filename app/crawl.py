f1e465b27cacbf66aabd95c54912844339ecf031
Alejandro Piad-Morffis <apiad@apiad.net>
2023-06-30 13:48:34 -0400

    Update index

--- app/crawl.py
+++ app/crawl.py
@@ -24,15 +24,15 @@ def get_data():
 def download_data():
     filepath = Path(__file__).parent / "data.csv"
 
-    if not filepath.exists() or time.time() - filepath.stat().st_mtime > 300:
+    if not filepath.exists() or time.time() - filepath.stat().st_mtime > 600:
         print("Downloading...")
 
         data = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vS6a7T0Lj4RctsmgCT-CJFKa_wMPe9Q7W_GxF9TmVqyfVDmXpZFmUh32LzMQV57C_ZbPDS6twBi2KbZ/pub?gid=138299737&single=true&output=csv',
-                        # Set first column as rownames in data frame
-                        index_col=0,
-                        # Parse column values to datetime
-                        parse_dates=['Timestamp']
-                        )
+            # Set first column as rownames in data frame
+            index_col=0,
+            # Parse column values to datetime
+            parse_dates=['Timestamp']
+        )
 
         with open(filepath, "w") as fp:
             data.to_csv(fp)
